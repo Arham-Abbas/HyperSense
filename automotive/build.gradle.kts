1f37ba0ce314a8bf2e39f4542a39941edbcaf567
Sayed Arham Abbas Rizvi <abbasrizvisayedarham6@gmail.com>
2024-04-22 01:48:06 +0530

    Initial Commit with all the files
    Signed-off-by: Sayed Arham Abbas Rizvi <abbasrizvisayedarham6@gmail.com>

---
+++ automotive/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.arham.hypersense"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.arham.hypersense"
+        minSdk = 32
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        signingConfig = signingConfigs.getByName("debug")
+        multiDexEnabled = true
+        testFunctionalTest = true
+        testHandleProfiling = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+    buildToolsVersion = "35.0.0 rc3"
+    ndkVersion = "27.0.11718014 rc1"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
