bd067aed1b9e24180043a8f0c9d1a5a2c3e7fdf0
Sayed Arham Abbas Rizvi <abbasrizvisayedarham6@gmail.com>
2024-04-24 13:23:07 +0530

    Refactored Code
    Moved the code into a single directory

--- automotive/src/main/java/com/arham/hypersense/NavigationSession.kt
+++ automotive/src/main/java/com/arham/hypersense/NavigationSession.kt
@@ -3,7 +3,6 @@ package com.arham.hypersense
 import android.content.Intent
 import androidx.car.app.Screen
 import androidx.car.app.Session
-import com.arham.hypersense.screen.MainScreen
 
 class NavigationSession : Session() {
     override fun onCreateScreen(intent: Intent) : Screen {
